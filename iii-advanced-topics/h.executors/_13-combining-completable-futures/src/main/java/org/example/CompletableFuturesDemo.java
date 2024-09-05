99e7dfd0acaa015d68486922e5e95ab6acf12411
Ela <micaelafrancisco13@outlook.com>
2024-09-05 13:58:03 +0800

    polish the module

--- iii-advanced-topics/h.executors/_13-combining-completable-futures/src/main/java/org/example/CompletableFuturesDemo.java
+++ iii-advanced-topics/h.executors/_13-combining-completable-futures/src/main/java/org/example/CompletableFuturesDemo.java
@@ -8,51 +8,30 @@ public class CompletableFuturesDemo {
         /*
          * CompletableFuture can start two tasks asynchronously and then combine the results.
          *
-         * For example, we query the Notes API and the NoteCategories API. When both async operations are completed, we combine
-         * the results to a JSON type like this:
-         *
-         * Note categories:
-         * {
-         *   "id": 1,
-         *   "name": "Work"
-         * }
-         *
-         * Notes:
-         * {
-         *   "name": "Note 1",
-         *   "category": "Work",
-         *   "description": "My work notes..."
-         * }
+         * For example, let's say we call a remote service, to get the price of a product, and that service returns the price in US
+         * dollars. So we get twenty US dollars. Now, at the same time, in parallel, we want to call another service to get the
+         * exchange rate between US dollars and Euros. So that service is going to return 0.9.
          *
          * We don't want to call the second service upon completion of the first task. We want to start this task concurrently and
          * then wait for both of them to complete to structure the result.
          *
          * */
-        var first1 = CompletableFuture.supplyAsync(() -> "Note category");
-        var second1 = CompletableFuture.supplyAsync(() -> "Note");
 
         // with thenCombine(), we can combine the result of 2 async operations.
-        first1
-                .thenCombine(second1, (noteCategory, note) -> {
-                    System.out.println(noteCategory);
-                    System.out.println(note);
-                    return "Note with category";
-                })
-                .thenAccept(System.out::println);
 
         /*
          * thenApply() is used in the context of CompletableFuture to process the result of a computation once it is completed. It
          * takes a function as an argument that operates on the result of the CompletableFuture, applies a transformation, and returns
          * a new result.
          * */
-        var first2 = CompletableFuture
+        var first = CompletableFuture
                 .supplyAsync(() -> "20USD")
                 .thenApply(price -> Integer.parseInt(price.replace("USD", "")));
 
-        var second2 = CompletableFuture.supplyAsync(() -> 0.9);
+        var second = CompletableFuture.supplyAsync(() -> 0.9);
 
-        first2
-                .thenCombine(second2, (price, exchangeRate) -> price * exchangeRate)
+        first
+                .thenCombine(second, (price, exchangeRate) -> price * exchangeRate)
                 .thenAccept(System.out::println);
 
         // All of this is happening asynchronously, so none of this code is going to block the current thread.
