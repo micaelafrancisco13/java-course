61099fc442ff36684c4864a7d80c1b771dabde8a
Ela <micaelafrancisco13@outlook.com>
2025-04-02 14:14:12 +0800

    add important comments

--- iii-advanced-topics/c.generics/_4-generic-classes/src/main/java/org/javacoursebymosh/GenericList.java
+++ iii-advanced-topics/c.generics/_4-generic-classes/src/main/java/org/javacoursebymosh/GenericList.java
@@ -9,7 +9,7 @@ public class GenericList<T> {
     // does not know the type of "T" at compile-time.
     // note that the line below produces ClassCastException.
     // why? see line 23 and onwards
-    private T[] items = (T[]) new Object[10];
+    private final T[] items = (T[]) new Object[10];
     private int count;
 
     public void add(T item) {
@@ -20,9 +20,20 @@ public class GenericList<T> {
         return items[index];
     }
 
-    // does not work
+    // Line 24 does not work.
     // Number number = new Object();
 
-    // works
+    // An instance of Object (created by new Object()) is not necessarily a Number.
+    // Although every Number is an Object, not every Object is a Number.
+    // The type system enforces that the right-hand side must be of type Number (or a
+    // subclass thereof) for this assignment to be valid. Since Object is a more general
+    // type, the compiler rejects this assignment.
+
+    // Line 33 works.
     // Object number = (Number) 10;
+
+    // The literal 10 is first autoboxed into an Integer (which is a subclass of Number).
+    // The cast (Number) 10 tells the compiler to treat the resulting Integer as a Number.
+    // Since Integer is indeed a Number, the cast is valid. Finally, assigning that Number
+    // to an Object is allowed because every Number is an Object.
 }
