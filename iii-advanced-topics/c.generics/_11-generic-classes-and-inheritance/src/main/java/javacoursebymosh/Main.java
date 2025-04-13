672c662540db1568668926fb1e159693f4310927
Ela <micaelafrancisco13@outlook.com>
2025-04-13 14:50:54 +0800

    WIP on main
    improve comments

--- iii-advanced-topics/c.generics/_11-generic-classes-and-inheritance/src/main/java/javacoursebymosh/Main.java
+++ iii-advanced-topics/c.generics/_11-generic-classes-and-inheritance/src/main/java/javacoursebymosh/Main.java
@@ -5,24 +5,22 @@ public class Main {
         User user1 = new User(10);
         User user2 = new Instructor(20);
 
-        // go to Utils class
-        Utils.printUser(user1);
-        Utils.printUser(user2);
-
-        // what if we expected a list of Users to print?
-        var users = new GenericList<>();
-        Utils.printUsers(users);
+        var users = new GenericList<>(User.class, 10);
 
         // what if we expected a list of Instructors to print?
-        var instructors = new GenericList<Instructor>();
-        // line above throws compilation error because a generic
-        // list of Instructor isn't a subtype of generic list of User.
-        // reason? type T parameters are converted to Object class,
-        // and the Object class isn't a subtype of itself
+        var instructors = new GenericList<>(Instructor.class, 10);
+        // users = instructors;
 
-        // one way to pass a list of Instructors
-//        var instructors = new GenericList<Instructor>();
-        // we can iterate through the list of instructors and add each
-        // instructor to the list of users. however, this is tedious
+        // Line 13 throws compilation error.
+        // Why? Because GenericList<Instructor> is not a subtype of
+        // GenericList<User>, even if Instructor extends User.
+
+        // Why? Because allowing that would break type safety.
+        // Imagine if this were allowed:
+        // GenericList<User> users2 = new GenericList<Instructor>(Instructor.class, 10);
+        users.add(new User(13)); // Adding a User to a list of Instructors!
+        // This would violate the list’s internal type constraint (it's supposed
+        // to only hold Instructor objects), which breaks the type safety Java
+        // promises with generics.
     }
 }
